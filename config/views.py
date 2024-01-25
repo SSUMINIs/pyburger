c1ddef9b2e51c44b044ec614620ad5fd8f8b6f91
“LeeSuMin” <tn9330@gmail.com>
2024-01-25 17:08:59 +0900

    UPDATED : new files

--- config/views.py
+++ config/views.py
@@ -4,10 +4,36 @@
 
 # from django.http import HttpResponse
 from django.shortcuts import render
+from burgers.models import Burger
+#import pandas as pd
 
 def main(request):
     #return HttpResponse("안녕하세요, pyburger입니다")
     return render(request, "main.html")
 def burger_list(request):
     #return HttpResponse("pyburger의 햄버거 목록입니다")
-    return render(request, "burger_list.html")
+    burgers = Burger.objects.all()
+    print("전체 햄버거 목록 : ", burgers)
+    context = {
+        "burgers":burgers,
+    }
+    return render(request, "burger_list.html", context)
+'''def burger_list(request):
+    burgers = Burger.objects.all().values()
+    data = pd.DataFrame(burgers)
+    context = {
+        "burgers":data.to_html(classes="table table-striped table-hover", index=False, justify="match-parent")
+    }
+    return render(request, "burger_list.html", context)'''
+def burger_search(request):
+    keyword = request.GET.get("keyword")
+    #print(keyword)
+    if keyword is not None:
+        burgers = Burger.objects.filter(name__contains=keyword)
+    else:
+        burgers = Burger.objects.none()
+    #print(burgers)
+    context = {
+        "burgers" : burgers,
+    }
+    return render(request, "burger_search.html",context)
